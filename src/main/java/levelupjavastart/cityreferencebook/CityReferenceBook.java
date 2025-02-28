4c1a238fd3972a0faa2643922b4c68e8f7c36dc1
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-02-28 20:00:05 +0300

    City Reference Book Improvements

--- src/main/java/levelupjavastart/cityreferencebook/CityReferenceBook.java
+++ src/main/java/levelupjavastart/cityreferencebook/CityReferenceBook.java
@@ -38,19 +38,18 @@ public class CityReferenceBook {
         System.out.println("Enter the population: ");
         Integer cityPopulation = scanner.nextInt();
         try (Connection con = DriverManager
-                .getConnection("jdbc:postgresql://localhost:5432/levelup", "root", "root")) {
-            try (PreparedStatement stmt = con.prepareStatement("""
-                    INSERT into cities (city_code, rus_name, eng_name, population) VALUES (?, ?, ?, ?);
-                    """)) {
-                stmt.setObject(1, cityCode);
-                stmt.setObject(2, russianCityName);
-                stmt.setObject(3, englishCityName);
-                stmt.setObject(4, cityPopulation);
-                if (stmt.executeUpdate() == 1) {
-                    System.out.printf("INFO: City %s was successfully saved \n", cityCode);
-                } else {
-                    System.out.printf("ERROR: City %s was not saved \n", cityCode);
-                }
+                .getConnection("jdbc:postgresql://localhost:5432/levelup", "root", "root");
+             PreparedStatement stmt = con.prepareStatement("""
+                     INSERT into cities (city_code, rus_name, eng_name, population) VALUES (?, ?, ?, ?);
+                     """)) {
+            stmt.setString(1, cityCode);
+            stmt.setString(2, russianCityName);
+            stmt.setString(3, englishCityName);
+            stmt.setInt(4, cityPopulation);
+            if (stmt.executeUpdate() == 1) {
+                System.out.printf("INFO: City %s was successfully saved \n", cityCode);
+            } else {
+                System.out.printf("ERROR: City %s was not saved \n", cityCode);
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
